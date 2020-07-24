6df4dcbad0de89c95a96e08e670359d8a84f3f23
xxl <2267298047@qq.com>
2020-07-24 21:59:32 +0800

    java记事本项目

---
+++ Text/src/util/SystemParam.java
@@ -0,0 +1,15 @@
+package util;
+
+/**
+ * ??????
+ * @author Yujie_Yang
+ *
+ */
+public class SystemParam {
+	
+	/**
+	 * ????????з?
+	 */
+	public static final String LINE_SEPARATOR=System.getProperty("line.separator");
+}
+
